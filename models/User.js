838db72fa1cd06b58f1aca925e7d3103495897cf
PaulomK <paulo.massao.kawakami@gmail.com>
2024-07-29 00:04:44 -0400

    Implement unique indexes to enforce unique 'username' and 'password'

--- models/User.js
+++ models/User.js
@@ -2,11 +2,15 @@ const mongoose = require("mongoose");
 const Resume = require("./Resume");
 
 const userSchema = new mongoose.Schema({
-  username: { type: String, required: true },
-  email: { type: String, required: true },
+  username: { type: String, required: true, index:{unique: true}},
+  email: { type: String, required: true, index:{unique: true} },
   password: { type: String, required: true },
   role: { type: String, required: true, enum: ["job_seeker", "employer"] },
   resume: Resume,
 });
 
-module.exports = mongoose.model("User", userSchema);
+const User = mongoose.model("User", userSchema);
+User.createIndexes(); // Ensure indexes are created -- will fail if current database has duplicated indexes
+module.exports = User;
+
+
