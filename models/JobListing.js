92f8c83dde2397365d154c1c9325bb9e737cb357
PaulomK <paulo.massao.kawakami@gmail.com>
2024-07-29 00:18:42 -0400

    define foreign key type as Object Id and add listingDate attribute with default value

--- models/JobListing.js
+++ models/JobListing.js
@@ -1,11 +1,12 @@
 const mongoose = require("mongoose");
 
 const joblistingSchema = new mongoose.Schema({
-  employerId: { type: String, required: true },
+  employerId: { type: mongoose.ObjectId, ref: "User", required: true },
   title: { type: String, required: true },
   description: { type: String, required: true },
   location: { type: String, required: true },
   salary: { type: Number, required: true },
+  listingDate: {type: Date, default: Date.now}
 });
 
 module.exports = mongoose.model("JobListing", joblistingSchema);
